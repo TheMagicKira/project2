4beb62ae27faf31c94b204be547733e7026e689a
Daniel Perrone <113133556+TheMagicKira@users.noreply.github.com>
2022-10-13 23:19:19 -0400

    Update todo.js

--- todo.js
+++ todo.js
@@ -17,12 +17,9 @@ task_status = task_status_inputs[i].value;
 let new_task_html = `<div class = "new-task-html"> <span> ${task_text} </span> <span>| ${task_priority} </span> <span> | ${task_status} </span> </div>
 
 <button class = "mark-complete"> Mark as Complete </button> <button class = "remove"> Remove </button>`;
-//let new_task_priority = ` <span> ${task_priority} </span>`;
-//let new_task_status = `<span> ${task_status} </span>`;
 
 li.innerHTML = new_task_html
 
-//li.innerHTML = document.querySelector('#task').value;
 tasks.push(task_text);
 document.querySelector('#task_list').append(li);
 document.querySelector('#task-title').value = '';
@@ -35,10 +32,8 @@ document.addEventListener('click', function(event){
 element = event.target;
 if (element.className === 'remove'){
 element.parentElement.remove();
-//tasks.remove(element);
 }
 if (element.className === 'mark-complete' || element.className === 'completed'){
-//document.querySelector('#task-title').strike();
 element.parentElement.querySelector('.new-task-html').style.textDecoration = "line-through";
 element.task_status = "completed";
 }
